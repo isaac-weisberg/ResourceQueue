2c7abac2292542cd0f3b5519e887ccf8efba98ae
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-04-20 12:37:28 +0700

    Add an xcode project

---
+++ ResourceQueue/ResourceQueue.swift
@@ -0,0 +1,7 @@
+//
+//  ResourceQueue.swift
+//  ResourceQueue
+//
+//  Created by Isaac Weisberg on 4/20/18.
+//  Copyright © 2018 isaac-weisberg. All rights reserved.
+//
---
+++ ResourceQueueTests/ResourceQueueTests.swift
@@ -0,0 +1,14 @@
+//
+//  ResourceQueueTests.swift
+//  ResourceQueueTests
+//
+//  Created by Isaac Weisberg on 4/20/18.
+//  Copyright © 2018 isaac-weisberg. All rights reserved.
+//
+
+import XCTest
+@testable import ResourceQueue
+
+class ResourceQueueTests: XCTestCase {
+    
+}
