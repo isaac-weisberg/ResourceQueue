a73e33387da3fed3cedc2a3378a4c424d04d3c4f
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-05-28 12:58:40 +0700

    Upgrade ResourceRetainer, improve code reusage

--- Sources/ResourceQueue/ResourceRetainer.swift
+++ Sources/ResourceQueue/ResourceRetainer.swift
@@ -10,6 +10,7 @@ import Foundation
 
 public class ResourceRetainer<Handle: Hashable, Resource> {
     public typealias Releaser = () -> Void
+    public typealias SimpleSetupper = (@escaping Releaser) -> Void
     public typealias Setupper = (Handle, Resource, @escaping Releaser) -> Void
     
     private var dictionary = [Handle: Resource]()
@@ -19,24 +20,26 @@ public class ResourceRetainer<Handle: Hashable, Resource> {
     }
     
     public func retain(_ resource: Resource, with handle: Handle, releaseSetup: @escaping Setupper) {
-        unhandling(handle) { releaser in
-            retain(resource, with: handle) {
-                releaseSetup(handle, resource, releaser)
-            }
+        prepare(resource, with: handle) { releaser in
+            releaseSetup(handle, resource, releaser)
         }
     }
     
-    public func retain(_ resource: Resource, with handle: Handle, simpleSetup: @escaping (@escaping Releaser) -> Void) {
-        unhandling(handle) { releaser in
-            retain(resource, with: handle) {
-                simpleSetup(releaser)
-            }
+    public func retain(_ resource: Resource, with handle: Handle, simpleSetup: @escaping SimpleSetupper) {
+        prepare(resource, with: handle) { releaser in
+            simpleSetup(releaser)
         }
     }
     
-    internal func retain(_ resource: Resource, with handle: Handle, doingStuff: (Releaser)? = nil) {
+    private func prepare(_ resource: Resource, with handle: Handle, _ actions: (@escaping Releaser) -> Void) {
+        unhandling(handle) { releaser in
+            retain(resource, with: handle)
+            actions(releaser)
+        }
+    }
+    
+    internal func retain(_ resource: Resource, with handle: Handle) {
         dictionary[handle] = resource
-        doingStuff?()
     }
     
     @discardableResult
