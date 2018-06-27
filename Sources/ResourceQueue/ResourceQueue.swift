7f074117583f4c2bc6b59bf1eaeddaea5125f11d
Isaac Weisberg <isaac_weisberg@outlook.com>
2018-06-27 19:04:54 +0700

    Separate resource queues into a protocol and two implementations of enqueue/dequeue Fix a call stack overflow

--- Sources/ResourceQueue/ResourceQueue.swift
+++ Sources/ResourceQueue/ResourceQueue.swift
@@ -6,23 +6,26 @@
 //  Copyright © 2018 isaac-weisberg. All rights reserved.
 //
 
-public class GenericResourceQueue<Handle: Hashable> {
+
+protocol ResourceQueueProtocol: class {
+    associatedtype Handle: Hashable
+
     typealias Entry = (Handle, Any)
 
-    internal var queue: [Entry] = []
-    internal var queueLength: Int
-    
-    public init(withLengthOf length: Int = 10) {
-        queueLength = length
-    }
-    
-    internal func recover() {
-        if queue.count > queueLength {
-            let diff = queue.count - queueLength
-            queue.removeSubrange(0..<diff)
-        }
-    }
-    
+    var queue: ArraySlice<Entry> { get set }
+
+    var queueLength: Int { get set }
+
+    func recover()
+
+    func setLength(_ length: Int)
+
+    func clean()
+
+    func remove(at handle: Handle)
+}
+
+extension ResourceQueueProtocol {
     ///
     /// Change the storage length, releasing oldest references to resources.
     ///
@@ -30,7 +33,43 @@ public class GenericResourceQueue<Handle: Hashable> {
         queueLength = length
         recover()
     }
+
+    internal func recover() {
+        if queue.count > queueLength {
+            let diff = queue.count - queueLength
+            queue.removeSubrange(0..<diff)
+        }
+    }
+
+    ///
+    /// Remove all strong references to enqueued resources.
+    ///
+    public func clean() {
+        queue.removeAll(keepingCapacity: true)
+    }
+
+    ///
+    /// Remove strong reference to an enqueued resource at handle.
+    ///
+    /// If there is no known resource enqueued at handle, nothing happens.
+    ///
+    public func remove(at handle: Handle) {
+        if let index = queue.index(where: {
+            $0.0 == handle
+        }) {
+            queue.remove(at: index)
+        }
+    }
+}
+
+public class GenericResourceQueue<Handle: Hashable>: ResourceQueueProtocol {
+    internal var queue: ArraySlice<(Handle, Any)> = ArraySlice([])
+    internal var queueLength: Int
     
+    public init(withLengthOf length: Int = 10) {
+        queueLength = length
+    }
+
     ///
     /// Enqueue a resource for strong retention at a hashable handle.
     ///
@@ -56,29 +95,16 @@ public class GenericResourceQueue<Handle: Hashable> {
         }
         return actualResource
     }
-    
-    ///
-    /// Remove all strong references to enqueued resources.
-    ///
-    public func clean() {
-        queue.removeAll(keepingCapacity: true)
-    }
-    
-    ///
-    /// Remove strong reference to an enqueued resource at handle.
-    ///
-    /// If there is no known resource enqueued at handle, nothing happens.
-    ///
-    public func remove(at handle: Handle) {
-        if let index = queue.index(where: {
-            $0.0 == handle
-        }) {
-            queue.remove(at: index)
-        }
-    }
 }
 
-public class ResourceQueue<Handle: Hashable, Resource>: GenericResourceQueue<Handle> {
+public class ResourceQueue<Handle: Hashable, Resource>: ResourceQueueProtocol {
+    var queue: ArraySlice<(Handle, Any)> = ArraySlice([])
+    var queueLength: Int
+
+    public init(withLengthOf length: Int = 10) {
+        queueLength = length
+    }
+
     ///
     /// Enqueue a resource for strong retention at a hashable handle.
     ///
@@ -87,13 +113,17 @@ public class ResourceQueue<Handle: Hashable, Resource>: GenericResourceQueue<Han
     ///     - handle: a handle to be used to remember the resource.
     ///
     public func enqueue(resource: Resource, for handle: Handle) {
-        return enqueue(resource: resource, for: handle)
+        queue.append((handle, resource))
+        recover()
     }
     
     ///
     /// Dequeue a resource at a hashable handle without removing it from strongly retained queue.
     ///
     public func dequeue(at handle: Handle) -> Resource? {
-        return dequeue(at: handle)
+        guard let res = queue.first(where: { $0.0 == handle })?.1 else {
+            return nil
+        }
+        return (res as! Resource)
     }
 }
